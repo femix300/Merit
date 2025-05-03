0f3e3daacebdb7d3a777392d9fd7ba91088aca5c
femix300 <oluwaseyipeter450@gmail.com>
2025-05-03 17:18:06 +0100

    updated the route in the post_utme_routes file

--- sql_backend/app/routes/post_utme_routes.py
+++ sql_backend/app/routes/post_utme_routes.py
@@ -4,6 +4,6 @@ from app.services.post_utme_service import determine_required_post_utme_score
 post_utme_bp = Blueprint('post_utme_bp', __name__)
 
 
-@post_utme_bp.route('/post-utme/required-score', methods=['GET'])
+@post_utme_bp.route('/post-utme/requirements', methods=['GET'])
 def determine_required_post_utme_score_route():
     return determine_required_post_utme_score()
