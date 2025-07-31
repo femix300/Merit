54531950617104d910737a047823de5a525baaf2
femix300 <oluwaseyipeter450@gmail.com>
2025-07-31 06:08:46 +0100

    commented out the code that creates all tables in models.py

--- sql_backend/app/models/models.py
+++ sql_backend/app/models/models.py
@@ -148,13 +148,13 @@ class FuoyeCourses(BaseUniCourses):
 
 
 # Create the tables
-if __name__ == '__main__':
-    try:
-        # Creates all tables based on the Base models
-        Base.metadata.create_all(engine)
-        print("Tables created successfully!")
-    except Exception as e:
-        print(f"Error creating tables: {e}")
+# if __name__ == '__main__':
+#     try:
+#         # Creates all tables based on the Base models
+#         Base.metadata.create_all(engine)
+#         print("Tables created successfully!")
+#     except Exception as e:
+#         print(f"Error creating tables: {e}")
 
 #session = Session()
 
